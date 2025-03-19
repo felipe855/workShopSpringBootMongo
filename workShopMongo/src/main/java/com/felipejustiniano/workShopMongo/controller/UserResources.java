beeba6ec78c600ca6a9bcf305789a5920445f8d8
Felipe Justiniano <felipejustiniano19@outlook.com>
2025-03-19 00:51:11 -0300

    insercao de usuario com post

---
+++ workShopMongo/src/main/java/com/felipejustiniano/workShopMongo/controller/UserResources.java
@@ -0,0 +1,45 @@
+package com.felipejustiniano.workShopMongo.controller;
+
+
+import com.felipejustiniano.workShopMongo.dto.UserDto;
+import com.felipejustiniano.workShopMongo.services.UserService;
+import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+import com.felipejustiniano.workShopMongo.domain.User;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @Autowired
+    private UserService service;
+
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<List<UserDto>> findAll() {
+        List<User> list = service.findAll();
+        List<UserDto> listDto = list.stream().map(x -> new UserDto(x)).toList();
+        return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value="/{id}",method=RequestMethod.GET)
+    public ResponseEntity<UserDto> findById(@PathVariable String id) throws BadRequestException {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDto(obj));
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDto objDto) {
+        User obj = service.fromDto(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+}
--- workShopMongo/src/main/java/com/felipejustiniano/workShopMongo/services/UserService.java
+++ workShopMongo/src/main/java/com/felipejustiniano/workShopMongo/services/UserService.java
@@ -1,11 +1,15 @@
 package com.felipejustiniano.workShopMongo.services;
 
 import com.felipejustiniano.workShopMongo.domain.User;
+import com.felipejustiniano.workShopMongo.dto.UserDto;
 import com.felipejustiniano.workShopMongo.repository.UserRepository;
+import com.felipejustiniano.workShopMongo.services.exception.ObjectNotFoundException;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +18,21 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) throws BadRequestException {
+        Optional<User> user = repo.findById(id);
+        if (user.isEmpty()) {
+            throw new BadRequestException("Objeto não encontado");
+        }
+        return user.get();
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDto(UserDto objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
